06597e3a006f68e3ffad18d7f49ad10c0da29565
JaeHoon Lee <hoonni2709@naver.com>
2022-02-03 13:45:04 +0900

    Init Study Vue Project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <keep-component />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import KeepComponent from "./components/comp/KeepComp.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    KeepComponent
   }
 }
 </script>
@@ -24,5 +23,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+    padding: 0;
+    margin: 0;
+}
+ul, ol {
+    list-style: none;
 }
 </style>
